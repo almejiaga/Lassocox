7fd9083c90d18f3e15d46305c216d3d8f32755de
almejiaga <124840761+almejiaga@users.noreply.github.com>
2024-04-29 18:12:39 -0500

    Update Lassocox_script.R
    changing the comments to make it more readable by users

--- Lassocox_script.R
+++ Lassocox_script.R
@@ -1,93 +1,81 @@
-#lasso-cox in R
-#Install the package and upload the image for running the algorithm
+# Load the RLassoCox package and required datasets
 BiocManager::install("RLassoCox")
 library(RLassoCox)
 data(mRNA_matrix)
 data(dGMMirGraph)
 data(survData) 
-#read the data, if you have a normalized counts matrix (by Deseq2 or EdgeR)
+
+# Read and prepare mRNA expression data
 library(readr)
 TCGA_UVM_htseq_counts <- read_delim("C:/Users/Genmol1/Downloads/TCGA-UVM.htseq_counts.tsv/TCGA-UVM.htseq_counts.tsv", 
                                     delim = "\t", escape_double = FALSE, 
                                     trim_ws = TRUE)
 expression <- as.data.frame(TCGA_UVM_htseq_counts)
-#add the IDs to rownames in case the structure of the file does not contain that
 rownames(expression) <- expression$Ensembl_ID
-#remove the ID column
 expressiondata <- expression[, 2:81]
-#scale the data, necessary for the analysis 
-DE_Z=t(scale(t(expressiondata)))
+DE_Z = t(scale(t(expressiondata)))
 expresionlista <- t(DE_Z)
-#expresionorganizada <- expresionlista[rownames(survivalcompleto), 1:100]
-# read the survival data
-library(readr)
+
+# Read and prepare survival data
 TCGA_UVM_survival_data <- read_delim("C:/Users/Genmol1/Downloads/TCGA-UVM.survival_data.txt", 
                                      delim = "\t", escape_double = FALSE, 
-                                     col_types = cols(OS = col_factor(levels = c("0", 
-                                                                                 "1")), OS.time = col_integer()), 
+                                     col_types = cols(OS = col_factor(levels = c("0", "1")), OS.time = col_integer()), 
                                      trim_ws = TRUE)
-#View(TCGA_UVM_survival_data)
-survival <-  as.data.frame(TCGA_UVM_survival_data)
-#convert 0s and 1s to FALSE and TRUE
+survival <- as.data.frame(TCGA_UVM_survival_data)
 survival$status <- ifelse(survival$OS == 0, "FALSE", "TRUE")
 survival$time <- survival$OS.time
-#create a subset of survival data with only outcome and time
 survivalcompleto <- survival[, c("status", "time")]
-#extract the samples IDs present in the survival dataframe
 rownames(survival) <- survival$sample
-#convert the outcome to a factor
 survivalcompleto$status <- as.logical(survivalcompleto$status)
-#estimate the intersection between expression matrix and survival table to check you have all samples in both
+
+# Check intersection between expression matrix and survival table
 intersect(colnames(TCGA_UVM_htseq_counts), rownames(survival))
-#you can run the LASSO cox for all the genes in the matrix. 
-#mod <- RLassoCox(x=expresionorganizada[,1:100], y=survivalcompleto, globalGraph=dGMMirGraph)
+
+# Run LASSO Cox regression with all genes
 mod <- RLassoCox(x=mRNA_matrix, y=survData, globalGraph=dGMMirGraph)
-#read your gene list for the analysis
+
+# Read gene list for analysis
 library(readxl)
-MEC_genes <- read_excel("C:/Users/Genmol1/Downloads/ECM TIMER2 (1).xlsx", 
-                        sheet = "final")
-#View(MEC_genes)
-#removing some info from the IDs (optional)
+MEC_genes <- read_excel("C:/Users/Genmol1/Downloads/ECM TIMER2 (1).xlsx", sheet = "final")
+
+# Filter expression matrix with gene list
 colnames(expresionorganizada) <- gsub("\\..*","",colnames(expresionorganizada))
-#filtering the expression matrix to include only our list of genes
 matrizMEC <- expresionorganizada[, MEC_genes$EnsemblID]                    
-#verify that the surival dataframe and expression matrix have the same individuals
 all((rownames(survivalcompleto) == rownames(matrizMEC)) == TRUE)
-#changing the IDs of the genes to allow running the algorithm
 MEC_genes$number <- seq(1:43)
 colnames(matrizMEC) <- MEC_genes$number
 mod3 <- RLassoCox(x=matrizMEC, y=survivalcompleto, globalGraph=dGMMirGraph)
 rownames(mRNA_matrix2) <- rownames(matrizMEC)
-#run the Lasso cox regression
-#checking the results
+
+# Check results
 mod3$PT
 head(coef(mod3$glmnetRes, s = 0.2))
-#performing crossvalidation with 5 folds
-cv.mod <- cvRLassoCox(x=matrizMEC, y=survivalcompleto,
-                      globalGraph=dGMMirGraph, nfolds = 5)
+
+# Perform cross-validation with 5 folds
+cv.mod <- cvRLassoCox(x=matrizMEC, y=survivalcompleto, globalGraph=dGMMirGraph, nfolds = 5)
+
+# Plot cross-validation results
 plot(cv.mod$glmnetRes, xlab = "log(lambda)")
 cv.mod$glmnetRes$lambda.min
 cv.mod$glmnetRes$lambda.1se
+
+# Identify non-zero coefficients
 coef.min <- coef(cv.mod$glmnetRes, s = "lambda.min")
-coef.min
 nonZeroIdx <- which(coef.min[,1] != 0)
 features <- rownames(coef.min)[nonZeroIdx]
-features
 features.coef <- coef.min[nonZeroIdx]
 names(features.coef) <- features
-coef <- features
+
+# Print features with non-zero coefficients
 features.coef
-#verifying the IDs that we changed
-lamatrizdeexpresion <- expresionorganizada[, MEC_genes$EnsemblID]
-plot(mod3$glmnetRes)
-print(mod3$glmnetRes)
+
+# Merge coefficient data with gene information
 coeficientesgenes <- as.data.frame(features.coef)
-mod3$PT
-#ahora uniendo los datos de los coeficientes y los genes
 coeficientesgenes$number <- rownames(coeficientesgenes)
 tablafinalLAssocox <- merge(coeficientesgenes, MEC_genes, by="number")
 write.table(tablafinalLAssocox, "coeficientesdelasso.txt", row.names = FALSE)
-#guardando los graficos en fullHD
+
+# Save plots
 png("grafico1LASSO.png", units="in", width=8, height=8, res=600)
 plot(cv.mod$glmnetRes, xlab = "log(lambda)")
 dev.off()
@@ -97,10 +85,12 @@ dev.off()
 png("grafico3LASSO.png", units="in", width=8, height=8, res=600)
 plot(mod3$glmnetRes, "lambda")
 dev.off()
-#guardando los dataframes por si algo
+
+# Save dataframes
 write.table(survivalcompleto, "supervivenciaLASSO.txt")
 write.table(MEC_genes, "informaciongenesLASSO.txt", row.names = FALSE)
 write.table(matrizMEC, "matriz_numeros_lasso.txt")
 write.table(lamatrizdeexpresion, "matriz_ensembl_ID_lasso.txt")
-#en el ensayo
+
+# In the essay
 head(coef(mod$glmnetRes, s = 0.2))
